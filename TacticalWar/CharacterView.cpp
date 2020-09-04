2824663c8984edde9e13d7959254d568ecafe59e
Alex6092 <alexandre.gremont@gmail.com>
2020-09-05 00:56:00 +0200

    Import SFML graphic library into client project

--- TacticalWar/CharacterView.cpp
+++ TacticalWar/CharacterView.cpp
@@ -1,1 +1,11 @@
 #include "CharacterView.h"
+
+
+sf::Sprite* CharacterView::getImageToDraw()
+{
+	// TODO : (team "Animation personnage") : Réaliser le code d'animation du personnage dans cette classe.
+	// Cette méthode (getImageToDraw) doit retourner l'image qui devra être dessinée à l'écran (en fonction de où en est l'animation)
+	// Vous devrez également utiliser les services de la team "Colorisation" pour obtenir un personnage coloré (personnalisation des couleurs en fonction de l'équipe).
+
+	return NULL;
+}
--- TacticalWar/CharacterView.h
+++ TacticalWar/CharacterView.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "AbstractCharacterView.h"
-
-class CharacterView : public AbstractCharacterView<CharacterView*>
+#include <SFML/Graphics/Sprite.hpp>
+class CharacterView : public AbstractCharacterView<sf::Sprite*>
 {
-
+public:
+	virtual sf::Sprite * getImageToDraw();
+	virtual void update(float deltatime);
 };
 
