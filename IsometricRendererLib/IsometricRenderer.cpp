c48aafe306c59d3459afe3e46fec824001ca2d8e
Alex6092 <alexandre.gremont@gmail.com>
2020-09-06 20:16:49 +0200

    Configure project to build for debug or release, add SFML lib in repository (compile with Visual Studio 2017 Community)

--- IsometricRendererLib/IsometricRenderer.cpp
+++ IsometricRendererLib/IsometricRenderer.cpp
@@ -8,5 +8,11 @@ void IsometricRenderer::render(Environment* environment, std::vector<BaseCharact
 	// TODO (team "Rendu graphique") : Réaliser le code de dessin de l'environnement (la map) et des personnages dans un repère isométrique.
 	// Vous devrez calculer la position des tuiles à l'écran en fonction de leur position dans la grille (dans le tableau 2D)
 
-	
+	for (int i = 0; i < environment->getWidth(); i++)
+	{
+		for (int j = 0; j < environment->getHeight(); j++)
+		{
+
+		}
+	}
 }
--- IsometricRendererTestModule/main.cpp
+++ IsometricRendererTestModule/main.cpp
@@ -4,14 +4,20 @@
 #include "BaseCharacterModel.h"
 #include <vector>
 #include <IsometricRenderer.h>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv)
 {
+	sf::Window window(sf::VideoMode(800, 600), "Isometric renderer test module");
+	
 	IsometricRenderer renderer;
 	Environment environment(20, 20);
 	std::vector<BaseCharacterModel*> characters;
 
-	renderer.render(&environment, characters);
+	while (true)
+	{
+		renderer.render(&environment, characters);
+	}
 
 	return 0;
 }
