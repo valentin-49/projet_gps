d7f52e7a7336684f1f672a415887393748212c4d
Alex6092 <julienlanglace@lapro.net>
2020-09-07 09:51:45 +0200

    Change dependencies path to be relative to the project folder ; add DLL in deployment folder

--- TacticalWar/main.cpp
+++ TacticalWar/main.cpp
@@ -1,13 +1,30 @@
 
-//#include "IsometricRenderer.h"
+#include "IsometricRenderer.h"
 
 int main(int argc, char** argv)
 {
-	//IsometricRenderer renderer;
-	//Environment environment(20, 20);
-	//std::vector<BaseCharacterModel*> characters;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Isometric renderer test module");
 
-	//renderer.render(&environment, characters);
+	IsometricRenderer renderer(&window);
+	Environment environment(20, 20);
+	std::vector<BaseCharacterModel*> characters;
+
+	environment.getMapData(2, 2)->setIsObstacle(true);
+	environment.getMapData(1, 1)->setIsWalkable(false);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		renderer.render(&environment, characters);
+		window.display();
+	}
 
 	return 0;
 }
