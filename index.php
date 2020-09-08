f3019231fe8366748f97b8e00dd637e77d90cc96
valentin_bouet <valbouet@outlook.fr>
2020-09-08 11:01:30 +0200

    first

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php require("user.php");?>
+<html>
+    <head>
+        <title>Le GPS du bled</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="bootstrap-4.4.1-dist/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <link rel="shortcut icon" href="image/unnamed.png" />
+    </head>
+<?php
+session_start();
+$_SESSION["isconnectUS"]=false;
+?>
+    <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 en-tete" align="center">
+                        <h1>Le GPS du bled</h1>
+                    </div>
+                    <div class="col-2 coterG" align="center">
+                        
+                    </div>
+                    <div class="col-1" align="center">
+                        
+                    </div>
+                    <div class="col-6 formulaire_de_connexion" align="center">
+                        <form action="index.php" method="POST">
+                            <label><h3>Se Connecter</h3></label>
+                            <p><input type="text" name="ID_1" width="auto" class="text" required/></p>
+                            <label><h3>Mot De Passe</h3></label>
+                            <p><input type="password" name="MDP_1" class="text" required/></p>
+                            <p><input type="submit" name="Valider" value="Valider" class="bouton"/></p>
+                        </form>
+                    <?php
+                       if(empty($_POST['ID_1']) && empty($_POST['MDP_1'])){
+
+                    }else{
+                    
+                    $user = new user();
+                    $_SESSION["isconnectUS"] = $user->Compar_passwd($_POST['ID_1'],$_POST['MDP_1']);
+                    if($_SESSION["isconnectUS"]){
+
+                        echo"<p><h3>Connexion à la BDD effectuer</h3></p>";
+                        $_SESSION["isconnectUS"]=true;
+
+                    }else{ //meesage d'erreur si les Id et Mdp sont incorrects
+                            
+                        echo"<p><h3>Identifiants ou mot de passe incorrects, veuillez reessayer.</h3></p>";
+                    }
+                    }
+                    ?>
+                    </div>
+                    <div class="col-1" align="center">
+                        
+                    </div>
+                    <div class="col-2 coterDr" align="center">
+                        
+                    </div>         
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        
+                        </div>
+                    <div class="col-2 " align="center">
+                            
+                        </div>
+                    <div class="col-8" align="center">
+                        <?php
+
+                        if($_SESSION["isconnectUS"]){
+                            ?>
+                                <div class="col-12 redirection" align="center">
+                                    <div class="row">
+                                        <div class="col-6" align="center"><p><input type="button" name="Valider" value="Accés à mon compte" onclick="self.location.href='../page_recherche/page_recherche.php'" class="bouton_redirect"/></p></div>                                      
+                                        <div class="col-6" align="center"><p><input type="button" name="Valider" value="Accés aux donnée" onclick=" self.location.href='../page_recherche/page_recherche.php'" class="bouton_redirect"/></p></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-6" align="center"> <p><input type="button" name="Valider" value="Position des bateaux" onclick="self.location.href='../page_recherche/page_recherche.php'" class="bouton_redirect"/></p></div>
+                                        <div class="col-6" align="center"><p><input type="button" name="Valider" value="Accées administrateur" onclick="self.location.href='acces_admin/admin.php'" class="bouton_redirect"/></p></div>
+                                    </div>
+                                        <div class="col-12" align="center"><p><input type="button" name="Valider" value="deconnexion" onclick="self.location.href='../page_recherche/page_recherche.php'" class="bouton_redirect"/></p></div>
+                                </div>
+                            <?php      
+                        }
+                        ?>
+                        </div>
+                    <div class="col-2 " align="center">
+                            
+                            </div>
+                </div>
+            </div>
+    </body>
+</html>
---
+++ user.php
@@ -0,0 +1,35 @@
+<?php
+class user{
+    //propriétes:
+    private $_user = "valentin";
+    private $_passwd = "root";
+    
+
+    //méthodes:
+   
+/*public function Connexion($NDC, $MDP){//Fonction qui permet au utilisateur de se connecter
+
+    try
+        {
+            $maBase=new PDO('mysql:host="localhost"; dbname="projet_tp_1";
+            charset=utf8','root', '');
+            $LesNVusers=$maBase->query('SELECT `ID`,`Mot_de_passe` FROM `Users` WHERE "'.$NDC.'"=`ID` && "'.$MDP.'"=`Mot_de_passe');
+            $admin = $LesNVusers->fetch();
+            $this->_nom = $admin['ID'];
+            $this->_MDP = $admin['Mot_de_passe'];
+        }    
+
+            catch (Exception $erreur){
+                echo 'Erreur : '.$erreur ->getMessage();
+        }
+}
+*/
+public function Compar_passwd($NDC, $MDP){ //compare les id et mdp 
+    if($NDC == $this->_user){
+        if($MDP == $this->_passwd){
+            return true;
+        }
+    }
+    return false;
+}
+}
