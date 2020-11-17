7c6c38b69d063c49ebecc7914e7226c2a41f1d74
florian <fwantelez@la-providence.net>
2020-11-17 21:23:31 +0100

    tuiles s'affiches bonne taille

--- IsometricRendererLib/IsometricRenderer.cpp
+++ IsometricRendererLib/IsometricRenderer.cpp
@@ -67,16 +67,62 @@ void IsometricRenderer::manageEvents(Environment * environment, std::vector<Base
 	}
 }
 
+void IsometricRenderer::test() {
+
+	sf::Texture textureGrass;
+	sf::Texture textureWater;
+	sf::Texture textureStone;
+	sf::Texture textureTree;
+
+	if (!textureGrass.loadFromFile("assets/tiles/Grass_01.png")) { std::cout << "Impossible de charger Grass texture" << std::endl; }
+	if (!textureWater.loadFromFile("assets/tiles/Water_01.png")) { std::cout << "Impossible de charger Water texture" << std::endl; }
+	if (!textureStone.loadFromFile("assets/tiles/Stone_02.png")) { std::cout << "Impossible de charger Stone texture" << std::endl; }
+	if (!textureTree.loadFromFile("assets/tiles/Tree_01.png")) { std::cout << "Impossible de charger Tree texture" << std::endl; }
+
+
+}
+
 void IsometricRenderer::render(Environment* environment, std::vector<BaseCharacterModel*> & characters, float deltatime)
 {
 	manageEvents(environment, characters);
 	// TODO (team "Rendu graphique") : Réaliser le code de dessin de l'environnement (la map) et des personnages dans un repère isométrique.
 	// Vous devrez calculer la position des tuiles à l'écran en fonction de leur position dans la grille (dans le tableau 2D)
 
+	sf::Texture textureGrass;
+	sf::Texture textureWater;
+	sf::Texture textureStone;
+	sf::Texture textureTree;
+
+
+		if (!textureGrass.loadFromFile("assets/tiles/Grass_0.png")) { std::cout << "Impossible de charger Grass texture" << std::endl; }
+
+	
+	//if (!textureWater.loadFromFile("assets/tiles/Water_01.png")) { std::cout << "Impossible de charger Water texture" << std::endl; }
+	//if (!textureStone.loadFromFile("assets/tiles/Stone_02.png")) { std::cout << "Impossible de charger Stone texture" << std::endl; }
+	//if (!textureTree.loadFromFile("assets/tiles/Tree_01.png")) { std::cout << "Impossible de charger Tree texture" << std::endl; }
+	
+	sf::Sprite spriteGrass;
+	sf::Sprite spriteStone;
+	sf::Sprite spriteWater;
+	sf::Sprite spriteTree;
+	sf::Sprite spriteToDraw;
+	
+	//sprite.setTextureRect(sf::IntRect(0, 0, 64, 64));
+	spriteGrass.setTexture(textureGrass);
+	spriteWater.setTexture(textureWater);
+	spriteStone.setTexture(textureStone);
+	spriteTree.setTexture(textureTree);
+	
+	
+
+	spriteGrass.setPosition(0, 0);
+	spriteGrass.setScale(0.05, 0.05);
+
+
 	sf::RectangleShape rect;
 	sf::Color color;
-	sf::Vector2f size(64, 64);
-	
+	sf::Vector2f size(32, 32);
+	/*
 	for (int i = 0; i < environment->getWidth(); i++)
 	{
 		for (int j = 0; j < environment->getHeight(); j++)
@@ -84,32 +130,39 @@ void IsometricRenderer::render(Environment* environment, std::vector<BaseCharact
 			CellData * cell = environment->getMapData(i, j);
 			if (cell->getIsObstacle())
 			{
-				color = sf::Color::Red;
+				//color = sf::Color::Red;
+				//spriteToDraw = spriteStone;
+
 			}
 			else if (cell->getIsWalkable())
 			{
-				color = sf::Color::White;
+				//color = sf::Color::White;
+				//spriteToDraw = spriteGrass;
 			}
 			else
 			{
-				color = sf::Color::Black;
-			}
-			
-			rect.setPosition(i*64, j*64); // peut être ici qu'il faut changer le co TODO:: Trouver la valeur de "environment->getWidth
-			rect.setSize(size);
 
-			sf::Color mulColor = sf::Color::White;
+				//color = sf::Color::Black;
+				//spriteToDraw = spriteWater;
+			}
+			spriteToDraw = spriteGrass;
+			spriteToDraw.setPosition(i*120, j*60); 
+			spriteToDraw.setScale(0.05, 0.05);
+			//sprite.setSize(size);
+
+			/*sf::Color mulColor = sf::Color::White;
 			if (colorator != NULL)
 			{
 				mulColor = colorator->getColorForCell(cell);
 			}
 
 			rect.setFillColor(color * mulColor);
-
-			window->draw(rect);
-		}
-	}
-	
+			*/
+			
+			window->draw(spriteGrass);
+		//}
+	//}
+	/*
 	for (int i = 0; i < characters.size(); i++)
 	{
 		BaseCharacterModel * m = characters[i];
@@ -117,14 +170,14 @@ void IsometricRenderer::render(Environment* environment, std::vector<BaseCharact
 		v.update(deltatime);
 		sf::Sprite * s = v.getImageToDraw();
 		s->setPosition(m->getInterpolatedX() * 64, m->getInterpolatedY() * 64);
-
+	
 
 		sf::IntRect rect = s->getTextureRect();
 		float scaleX = 64.0 / (float)rect.width;
 		float scaleY = 64.0 / (float)rect.height;
 		s->setScale(scaleX, scaleY);
 		window->draw(*s);
-	}
+	}*/
 }
 
 
